3fe3ec4ba257d33e46f804ca148b1f35a3ba1a99
noxob <samirusta18@gmail.com>
2022-01-22 14:38:06 +0300

    removed unnecessary comments

--- src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
+++ src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
@@ -10,21 +10,6 @@ public class TodoappApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
-//		int n = 529;
-//		String binary = Integer.toString(n,2);
-//		
-//		int longestZeroSize = 0;
-//		
-//		String[] zeros = binary.split("1");
-//		
-//		for(int i = 0; i < zeros.length; i++) {
-//			
-//			if(zeros[i].length() > longestZeroSize) {
-//				longestZeroSize = zeros[i].length();
-//			}
-//			
-//		}
-		
 	}
 
 }
