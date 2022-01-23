ef9aded55bb54e18f260bc3ab63ab273fb0e0d4c
noxob <samirusta18@gmail.com>
2022-01-23 19:49:50 +0300

    removed unused imports

--- src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
+++ src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
@@ -2,9 +2,7 @@ package com.ismailsamirusta.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
