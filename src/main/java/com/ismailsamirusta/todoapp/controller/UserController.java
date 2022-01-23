04461440bd7d0e745542aae355494312fd5f1951
noxob <samirusta18@gmail.com>
2022-01-23 14:09:08 +0300

    implemented user registration method

--- src/main/java/com/ismailsamirusta/todoapp/controller/UserController.java
+++ src/main/java/com/ismailsamirusta/todoapp/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.ismailsamirusta.todoapp.controller;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,11 +21,23 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	
 	@PostMapping("/user/register")
-	public void registerUser(@RequestBody AppUser user) {
-		userService.addUser(user);
+	public ResponseEntity<Map<String,String>> registerUser(@RequestBody AppUser user) {
+		Map<String,String> responseMap = new HashMap<String,String>();
+		HttpStatus status = HttpStatus.OK;
+		try {
+			userService.addUser(user);
+			responseMap.put("success", "true");
+		}catch(Exception e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			responseMap.put("success", "false");
+			responseMap.put("error", e.getMessage());
+		}
+		return new ResponseEntity<Map<String,String>>(responseMap, status);
 	}
 	
-	
+	@GetMapping("/user/myuser")
+	public ResponseEntity<AppUser> getMyUser(Principal principal){
+		return new ResponseEntity<AppUser>(userService.findByUsername(principal.getName()), HttpStatus.OK);
+	}
 }
