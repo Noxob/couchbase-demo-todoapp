94acf74373d3549fef620eef2569d24d6775ba74
noxob <samirusta18@gmail.com>
2022-01-23 14:06:12 +0300

    added bcrypt for password encryption

--- src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
+++ src/main/java/com/ismailsamirusta/todoapp/TodoappApplication.java
@@ -2,7 +2,9 @@ package com.ismailsamirusta.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
--- src/main/java/com/ismailsamirusta/todoapp/service/UserServiceImpl.java
+++ src/main/java/com/ismailsamirusta/todoapp/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ismailsamirusta.todoapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ismailsamirusta.todoapp.model.AppUser;
@@ -10,16 +11,23 @@ import com.ismailsamirusta.todoapp.repository.UserRepository;
 public class UserServiceImpl implements UserService{
 
 	@Autowired
-	private UserRepository UserRepository;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypto;
+	
+	private final String USER_ID_PREFIX="USER:";
 	
 	@Override
 	public void addUser(AppUser user) {
-		UserRepository.save(user);
+		user.setId(USER_ID_PREFIX+user.getUsername());
+		user.setPassword(bcrypto.encode(user.getPassword()));
+		userRepository.save(user);
 	}
 
 	@Override
 	public AppUser findByUsername(String username) {
-		return UserRepository.findByUsername(username);
+		return userRepository.findByUsername(username);
 	}
 		
 }
